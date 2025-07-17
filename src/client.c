0e3555b55af2b2bbff14e185086bbfaaef97db05
Ушаков Александр Вадимович <Ushakov.AV@dns-shop.ru>
2025-07-17 12:46:39 +1000

    debug

--- src/client.c
+++ src/client.c
@@ -575,7 +575,7 @@ bool set_pool(PgSocket *client, const char *dbname, const char *username, const 
 			return false;
 		}
 		/* Password will be set after successful authentication when not in takeover mode */
-		client->login_user_credentials = add_user(username, password);
+		client->login_user_credentials = add_user_credentials(username, password);
 		if (!client->login_user_credentials) {
 			slog_error(client, "set_pool(): failed to allocate new LDAP user");
 			disconnect_client(client, true, "bouncer resources exhaustion");
